4e042678bdd04750fac3ff194aa0cc6bf3a1a45d
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-07-09 15:08:24 +0700

    Album Versioning & Change Controller to V1

--- app/Http/Controllers/V1/AlbumController.php
+++ app/Http/Controllers/V1/AlbumController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\V1;
 
+use App\Http\Controllers\Controller;
 use App\Models\Album;
 use App\Http\Requests\StoreAlbumRequest;
 use App\Http\Requests\UpdateAlbumRequest;
